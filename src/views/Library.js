2646564e45bc0c7a7105ea7dfd00fce0189540c6
Alejandro García <alejandro.garci.rodr@gmail.com>
2020-05-06 11:57:48 +0200

    first

---
+++ src/views/Library.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Library = () => {
+  return (
+    <View>
+      <Text>hola soy la vista libary</Text>
+    </View>
+  );
+};
+
+export default Library;
